b41a797eece48b1196378fb08cdfd5021315214b
Andrey <derevishko@gmail.com>
2020-11-05 12:45:49 +0300

    update main.js for homework 1

--- homework/homework_1/main.js
+++ homework/homework_1/main.js
@@ -1,14 +1,16 @@
-console.log("hello"); {
-    let tmp;
-  
-    for (let i = index.length - 1; i > 0; i--) {
-        for (let j = 0; j < i; j++) {
-            if (index[j] > index[j+1]) {
-                tmp = index[j];
-                index[j] = index[j+1];
-                index[j+1] = tmp;
-            }
-        }
-    }
-    return index;
+let array = [12, 24, 1, 12, 421,5,1,-1,23,-71,32];
+
+let arrayAfterSort = bubleSort(array); // вызов функции и получение результата
+
+console.log(arrayAfterSort); // вывод результата
+
+let resultMessage = test("Роис", "иди нахуй");
+
+
+function bubleSort(array) { 
+    return array;
+}
+
+function test(name, message) { 
+    return name + ' ' + message;
 }
